aa8a6c26185347faf80090f64fb6d7a26c0ce46f
Risto Hietala <risto.hietala@gmail.com>
2018-09-19 23:23:56 +0300

    pythagorean-triplet more efficient solution

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,20 +1,22 @@
 pub fn find() -> Option<u32> {
     for a in 1..1000 {
-        for b in 1..1000 {
-            for c in 1..1000 {
-                if rule1(a, b, c) && rule2(a, b, c) {
-                    return Some(a * b * c);
-                }
+        for b in 1..a {
+            // a + b + c == 1000, therefore
+            let c = 1000 - a - b;
+
+            // sum_equals_1000 check should always hold
+            if is_pythagorean_triplet(a, b, c) && sum_equals_1000(a, b, c) {
+                return Some(a * b * c);
             }
         }
     }
     None
 }
 
-pub fn rule1(a: u32, b: u32, c: u32) -> bool {
+pub fn is_pythagorean_triplet(a: u32, b: u32, c: u32) -> bool {
     a*a + b*b == c*c
 }
 
-pub fn rule2(a: u32, b: u32, c: u32) -> bool {
+pub fn sum_equals_1000(a: u32, b: u32, c: u32) -> bool {
     a + b + c == 1000
 }
