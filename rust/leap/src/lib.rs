f05dc64ededc290ebdfa9af23f286b63c9cce551
Risto Hietala <risto.hietala@gmail.com>
2018-09-24 22:47:40 +0300

    leap

---
+++ rust/leap/src/lib.rs
@@ -0,0 +1,11 @@
+// on every year that is evenly divisible by 4
+// except every year that is evenly divisible by 100
+// unless the year is also evenly divisible by 400
+pub fn is_leap_year(year: i32) -> bool {
+    match year {
+        _ if (year % 400 == 0) => true,
+        _ if (year % 100 == 0) => false,
+        _ if (year % 4 == 0) => true,
+        _ => false
+    }
+}
