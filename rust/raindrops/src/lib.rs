2469225e1d6b2c1fd37bad9bec84985d46db1929
Risto Hietala <risto.hietala@gmail.com>
2018-09-25 09:13:35 +0300

    raindrops better solution

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,24 +1,24 @@
 // https://doc.rust-lang.org/1.9.0/book/strings.html
 
+extern crate itertools;
 use itertools::Itertools;
 
-pub fn fizzbuzzer(n: u32, divisor: u32, s: String) -> Option<String> {
+pub fn fizzbuzzer(n: u32, divisor: u32, s: &str) -> Option<String> {
     if n % divisor == 0 {
-        Some(s)
+        Some(String::from(s))
     } else {
         None
     }
 }
 
 pub fn raindrops(n: u32) -> String {
-    let drops = [fizzbuzzer(n, 3, "Pling".to_string()),
-                 fizzbuzzer(n, 5, "Plang".to_string()),
-                 fizzbuzzer(n, 7, "Plong".to_string())];
-    match drops {
-        [None, None, None] => n.to_string(),
-        _ => drops
+    match (fizzbuzzer(n, 3, "Pling"),
+           fizzbuzzer(n, 5, "Plang"),
+           fizzbuzzer(n, 7, "Plong")) {
+        (None, None, None) => n.to_string(),
+        (a, b, c) => [a, b, c]
             .iter()
-            .map(|x| x.unwrap_or("".to_string()))
+            .map(|x| x.clone().unwrap_or(String::from("")))
             .join("")
     }
 }
